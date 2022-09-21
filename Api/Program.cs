413b2c1217c486e6e452290a546eb2145f2bf682
HDMC3 <moranh56@gmail.com>
2022-09-21 06:41:33 -0600

    Initial commit

---
+++ Api/Program.cs
@@ -0,0 +1,27 @@
+namespace Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapGet("/", () => "Hello world!");
+
+        app.Run();
+    }
+}
