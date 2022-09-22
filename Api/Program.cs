3ab448d03f01f9cb9add5f36ce55f625bd104780
HDMC3 <moranh56@gmail.com>
2022-09-21 19:08:48 -0600

    Add database context service

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 namespace Api;
 
 public class Program
@@ -8,6 +11,8 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+
+        builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlite());
 
         var app = builder.Build();
 
