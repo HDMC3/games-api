190946e42d6452ac1d8007cff1f3e7b79274a460
HDMC3 <moranh56@gmail.com>
2022-09-26 01:45:19 -0600

    Fix web property type

--- Aplication/Queries/Games/DTOs/GameSoundtrackDto.cs
+++ Aplication/Queries/Games/DTOs/GameSoundtrackDto.cs
@@ -1,10 +1,10 @@
 namespace Aplication.Queries.Games.DTOs;
 
 public class GameSoundtrackDto {
-    public string web { get; set; }
+    public string? web { get; set; }
     public string url { get; set; }
     
-    public GameSoundtrackDto(int id, string web) {
+    public GameSoundtrackDto(int id, string? web) {
         this.web = web;
         url = $"/soundtracks?id={id}";
     }
