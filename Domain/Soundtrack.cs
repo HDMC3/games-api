4e30f79ebc6fdf5b24f3af622a75b8e00712f461
HDMC3 <moranh56@gmail.com>
2022-09-26 01:14:05 -0600

    Fix web property type

--- Domain/Soundtrack.cs
+++ Domain/Soundtrack.cs
@@ -5,12 +5,12 @@ public class Soundtrack {
     
     public int Id { get; private set; }
     public string Name { get; set; }
-    public string Web { get; set; }
+    public string? Web { get; set; }
     public string Composer { get; set; }
     public int GameId { get; set; }
     public Game Game { get; set; }
 
-    public Soundtrack(string web, string name, string composer, Game game) {
+    public Soundtrack(string? web, string name, string composer, Game game) {
         Web = web;
         Game = game;
         Name = name;
