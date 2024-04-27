6c01171874294ac8e6b9c398f299647e590599b1
Hugo Moran <moranh56@gmail.com>
2024-04-26 18:38:35 -0600

    Change api title

--- Api/Program.cs
+++ Api/Program.cs
@@ -31,7 +31,7 @@ public class Program
         {
             options.SwaggerDoc("v1", new OpenApiInfo
             {
-                Title = "Schedule App API"
+                Title = "Games API"
             });
         });
 
